e8e3f1a5393dd4a981aa2bba312a8290c53c5d0f
Aram Dulyan <aram00@gmail.com>
2013-01-26 18:40:06 +1100

    Documentation++. Even though time is short, I've always regretted not documenting.

--- beast.js
+++ beast.js
@@ -4,25 +4,34 @@ var boxes = [];
 var cattery, looper, playarea;
 
 function random(min, max) {
+	// Returns random number (not necessarily integer) between the limits
 	return Math.random() * (max - min) + min;
 }
 
 function get_px(element, attribute) {
+	// A helper function to return the numeric value in one of the elements
+	// positioning style attributes, such as "left" or "bottom". Uses the
+	// computed style, which is always in pixels.
 	style = window.getComputedStyle(element);
+	// Strip the "px" suffix and convert into a number
 	return Number(style[attribute].substring(0, style[attribute].length - 2));
 }
 
 function make_new_box() {
+	// Randomise initial box position
 	var y_offset = Math.floor(random(0, 3));
 	var x_offset = random(10, 80);
+	// Create the box and the heartbeat in the DOM
 	var box_div = document.createElement('div');
-	var heartbeat_div = document.createElement('div');
 	box_div.className = 'box';
 	box_div.style.zIndex = 3 - y_offset;
 	box_div.style.left = x_offset + '%';
 	box_div.style.bottom = y_offset * 33.33 + '%';
+	var heartbeat_div = document.createElement('div');
+	heartbeat_div.className = 'heartbeat';
 	cattery.appendChild(box_div);
 	box_div.appendChild(heartbeat_div);
+	// Create the box meta object for future reference
 	var box = {
 		'element': box_div,
 		'x': x_offset,
@@ -32,21 +41,25 @@ function make_new_box() {
 		'fading': false,
 	};
 	boxes.push(box);
+	// Assign event listeners
 	box_div.onclick = function(){ launch_cat(box); };
 }
 
 function launch_cat(box) {
+	// Create the cat in the DOM
 	var cat_div = document.createElement('div');
 	cat_div.className = 'cat';
-	cat_div.style.left = get_px(box.element, 'left') + 80 + 50 + 'px';
+	cat_div.style.left = get_px(box.element, 'left') + 37.5 + 80 + 'px';
 	cat_div.style.bottom = get_px(box.element, 'bottom') + get_px(cattery, 'bottom') + 75 + 'px';
 	playarea.appendChild(cat_div);
+	// Create the cat meta object for future reference
 	var cat = {
 		'element': cat_div,
 		'velocity': 40.0,
 		'z_index': box.element.style.zIndex,
 	};
 	cats.push(cat);
+	// Start the box removal process
 	box.fading = true;
 	box.element.onclick = null;
 }
@@ -54,20 +67,26 @@ function launch_cat(box) {
 function fly_cat(cat, index, array) {
 	var old_velocity = cat.velocity;
 	var position = get_px(cat.element, 'bottom');
+	// Move the cat based on its current velocity and gravity
 	position += cat.velocity / 3.0;
 	cat.velocity -= 9.83 / 3.0;
 	cat.element.style.bottom = position + 'px';
+	// When the cat passes the peak of its trajectory, make sure it appears
+	// on top of its box as it comes down
 	if(old_velocity >= 0.0 && cat.velocity < 0)
 		cat.element.style.zIndex = cat.z_index + 1;
 }
 
 function update_box(box, index, array) {
+	// Fade out fading boxes
 	if(box.fading) {		
 		if(!box.element.style.opacity)
+			// Don't start fading until the cat has cleared it
 			box.element.style.opacity = 4.0;
 		else
 			box.element.style.opacity -= 0.1;
 	}
+	// Remove boxes that have faded away from the DOM and memory
 	if(box.element.style.opacity < 0) {
 		boxes.splice(boxes.indexOf(box), 1);
 		box.element.parentNode.removeChild(box.element);
@@ -76,14 +95,19 @@ function update_box(box, index, array) {
 }
 
 function event_loop() {
+	// The current level determines how many boxes appear at one time
 	if(boxes.length < level)
 		make_new_box();
+	// Move any airborne cats through the air
 	cats.forEach(fly_cat);
+	// Update heartbeats and fade out fading boxes
 	boxes.forEach(update_box);
 }
 
 window.onload = function() {
+	// Load elements against which things will be positioned into globals
 	cattery = document.getElementById('cattery');
 	playarea = document.getElementById('playarea');
+	// Enter event loop
 	looper = window.setInterval(event_loop, 33);
 };
--- minify/minify.py
+++ minify/minify.py
@@ -6,13 +6,53 @@ import StringIO
 import subprocess
 import sys
 
-names_re = re.compile(r'([#\.][\w\-]+)(?=[^}]*{)', re.M)
+# Matches text outside HTML tags, removing non-space whitespace, and
+# removing spaces adjacent to tags
 outside_tags = re.compile(r'\s*>[^<]+<\s*', re.M)
-inside_tags = re.compile(r'<[^>]+>', re.M)
-attributes = re.compile(r'(class|id)\s*=\s*[\'"](.*?)[\'"]')
-link_tag = re.compile(r'<link[^>]+href\s*=\s*[\'"](.*?)[\'"][^>]*>', re.M)
-script_tag = re.compile(r'<script[^>]+src\s*=\s*[\'"](.*?)[\'"][^>]*>', re.M)
+def strip_whitespace(match):
+    stripped = match.group(0).replace('\t', '').replace('\n', '')
+    return re.sub(r'\s+<', '<', re.sub(r'>\s+', '>', stripped))
 
+# Matches .class or #id names in CSS and reduces them to their first letter
+names_re = re.compile(r'([#\.][\w\-]+)(?=[^}]*{)', re.M)
+def trim_css_names(match):
+    return match.group(1)[:2]
+
+# Matches class or id declarations inside HTML tags and reduces them to their
+# first letter
+attributes = re.compile(r'(class|id)\s*=\s*[\'"](.*?)[\'"]')
+def trim_attributes(match):
+    names = [n[0] for n in match.group(2).split(' ')]
+    return '%s=\'%s\'' % (match.group(1,), ' '.join(names))
+
+# Matches the insides of HTML tags and passes it on to trim_attributes above
+# to trim class and id names
+inside_tags = re.compile(r'<[^>]+>', re.M)
+def trim_html_names(match):
+    return attributes.sub(trim_attributes, match.group(0))
+
+# Matches a <link> lag with a "href" and inlines the linked CSS, minifying
+# it first. Note that this assumes that all class and id names start with
+# unique letters.
+link_tag = re.compile(r'<link[^>]+href\s*=\s*[\'"](.*?)[\'"][^>]*>', re.M)
+def compress_and_inline_css(match):
+    style_path = os.path.join(os.path.dirname(sys.argv[1]), match.group(1))
+    with open(style_path) as style_file:
+        style = style_file.read()
+        # Trim calss and id names to their first letter
+        trimmed_style = names_re.sub(trim_css_names, style)
+    # Minify with YUI
+    compress_command = subprocess.Popen(
+        ['java', '-jar', 'yuicompressor-2.4.8pre.jar', '--type', 'css'],
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+    )
+    compressed,_ = compress_command.communicate(trimmed_style)
+    return '<style>%s</style>' % compressed
+
+# Matches a <script> tag with an "src" and inlines the JS, minifying it first.
+# Note that as above, class and id names are assumed to start with unique
+# letter. Since YUI doesn't touch global variables, we minify them using the
+# mapping below first.
 script_replace = (
     ('boxes', 'x'),
     ("'box'", "'b'"),
@@ -24,40 +64,15 @@ script_replace = (
     ('looper', 'l'),
     ('level', 'v'),
 )
-
-def strip_whitespace(match):
-    stripped = match.group(0).replace('\t', '').replace('\n', '')
-    return re.sub(r'\s+<', '<', re.sub(r'>\s+', '>', stripped))
-
-def trim_css_names(match):
-    return match.group(1)[:2]
-
-def trim_attributes(match):
-    names = [n[0] for n in match.group(2).split(' ')]
-    return '%s=\'%s\'' % (match.group(1,), ' '.join(names))
-
-def trim_html_names(match):
-    return attributes.sub(trim_attributes, match.group(0))
-
-def compress_and_inline_css(match):
-    style_path = os.path.join(os.path.dirname(sys.argv[1]), match.group(1))
-    with open(style_path) as style_file:
-        style = style_file.read()
-        trimmed_style = names_re.sub(trim_css_names, style)
-        
-    compress_command = subprocess.Popen(
-        ['java', '-jar', 'yuicompressor-2.4.8pre.jar', '--type', 'css'],
-        stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-    )
-    compressed,_ = compress_command.communicate(trimmed_style)
-    return '<style>%s</style>' % compressed
-
+script_tag = re.compile(r'<script[^>]+src\s*=\s*[\'"](.*?)[\'"][^>]*>', re.M)
 def compress_and_inline_js(match):
     script_path = os.path.join(os.path.dirname(sys.argv[1]), match.group(1))
     with open(script_path) as script_file:
         script = script_file.read()
+        # Minify the things that YUI won't
         for key, value in script_replace:
             script = script.replace(key, value)
+    # Minify with YUI
     compress_command = subprocess.Popen(
         ['java', '-jar', 'yuicompressor-2.4.8pre.jar', '--type', 'js'],
         stdin=subprocess.PIPE, stdout=subprocess.PIPE,
@@ -65,6 +80,7 @@ def compress_and_inline_js(match):
     compressed,_ = compress_command.communicate(script)
     return '<script>%s' % compressed
 
+# The main running block. Minifies the HTML file passed as argument.
 with open(sys.argv[1]) as html_file:
     html = html_file.read()
     html = outside_tags.sub(strip_whitespace, html)
